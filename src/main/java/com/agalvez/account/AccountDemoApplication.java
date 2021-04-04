0b059731b58d2e65240b555ec25f82e1104d428f
Adrian Clavijo <adrclavijo@gmail.com>
2021-04-04 19:26:38 +0200

    First commit

---
+++ src/main/java/com/agalvez/account/AccountDemoApplication.java
@@ -0,0 +1,13 @@
+package com.agalvez.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountDemoApplication.class, args);
+	}
+
+}
